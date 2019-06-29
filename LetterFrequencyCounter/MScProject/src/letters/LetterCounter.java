120bf9d7709237f3f1c49ca1ac1e8649fab5393e
Lilly Schwarzenbach <lillyschwarzenbach@gmail.com>
2019-06-29 14:49:29 +0400

    Reduce FrequencyCounter to only include sequences of 2 letters; add direction detection and timers to Aseba code; add blue light if no neighbours

--- LetterFrequencyCounter/MScProject/src/letters/LetterCounter.java
+++ LetterFrequencyCounter/MScProject/src/letters/LetterCounter.java
@@ -17,10 +17,7 @@ public class LetterCounter {
 	int dictionary_size;
 	
 	public LetterCounter() {
-		twoLetterFrequencies = determineFrequenciesTwoLetters("C:\\Users\\lschw\\Dropbox\\Bham\\Swarm robotics\\LetterFrequencyCounter\\MScProject\\src\\letters\\Set3.txt");
-//		threeLetterFrequencies = determineFrequenciesThreeLetters("C:\\Users\\lschw\\Dropbox\\Bham\\Swarm robotics\\LetterFrequencyCounter\\MScProject\\src\\letters\\Set3.txt");
-//		dictionary_size = countDicEntries("C:\\Users\\lschw\\Dropbox\\Bham\\Swarm robotics\\LetterFrequencyCounter\\MScProject\\src\\letters\\Set3.txt");
-//		determineFrequenciesAbc("C:\\Users\\lschw\\Dropbox\\Bham\\Swarm robotics\\LetterFrequencyCounter\\MScProject\\src\\letters\\Set3.txt");
+		twoLetterFrequencies = determineFrequenciesTwoLetters("src\\letters\\Set3.txt");
 		
 		hm = new HashMap<>();
 		hm.put("a", 0);
@@ -61,46 +58,7 @@ public class LetterCounter {
 		}
 		return occurrences;
 	}
-	
-	public static int countDicEntries(String dictionaryfile) {
-		Scanner s;
-		String st;
-		int no = 0;
-		try {
-			s = new Scanner(new File(dictionaryfile));
-			while(s.hasNext()) {
-				st = s.next();
-				no++;
-			}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return no;
-	}
-	
 
-	public void determineFrequenciesAbc(String filename){
-		String letter;
-		int occurs;
-		abcFrequencies = new int[27];
-		Scanner s;
-		for(int i = 0; i <27; i++) {
-			try {
-				s = new Scanner(new File(filename));
-				occurs = 0;
-				letter = abc[i];
-				while(s.hasNext()) {
-					occurs += countRegEx(s.nextLine(), letter);
-				}
-				abcFrequencies[i] = occurs;
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-				
-		}
-	}
 	
 	public int[][] determineFrequenciesTwoLetters(String filename){
 		int[][] frequencies = new int[27][27];
@@ -139,73 +97,24 @@ public class LetterCounter {
 	}
 	
 	
-	public int[][][] determineFrequenciesThreeLetters(String filename){
-		int[][][] frequencies = new int[27][27][27];
-		String combo;
-		int occurs;
-		Scanner s;
-		String test;
-		for(int i = 0; i <27; i++) {
-			for(int j = 0; j<27; j++) {
-				for(int k = 0; k<27; k++) {
-					try {
-						s = new Scanner(new File(filename));
-						occurs = 0;
-						combo = abc[i] + abc[j] + abc[k];
-						while(s.hasNext()) {
-							test = " " + s.nextLine() + " ";
-							occurs += countRegEx(test, combo);
-						}
-						frequencies[i][j][k] = occurs;
-						if(occurs > 0) {
-							if(abc[i].equals(" ")) {
-								combo = "[]" + abc[j] + abc[k];
-							}
-							else if(abc[k].equals(" ")) {
-								combo = abc[i] + abc[j] + "[]";
-							}
-							System.out.println(combo + " : " + occurs);
-						}
-					} catch (FileNotFoundException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-				
-			}
-		}
-		return frequencies;
-	}
-	
-	
 	public int happiness(String currentConfig) {
 		
 
 		int prevHappy;
-		int endHappy;
-		int initHappy;
 		int fullHappy;
 		
 		if(currentConfig.length() == 2) {
 			int first = hm.get(String.valueOf(currentConfig.charAt(0)));
-			int second = hm.get(String.valueOf(currentConfig.charAt(1)));
-			
-			initHappy = twoLetterFrequencies[26][first];
-			prevHappy = twoLetterFrequencies[first][second];
-			endHappy = twoLetterFrequencies[second][26];
-			
-//			return (int)((prevHappy/Math.max(abcFrequencies[first], abcFrequencies[second])) * 100);
-			return prevHappy;
-			
+			int second = hm.get(String.valueOf(currentConfig.charAt(1)));			
+			prevHappy = twoLetterFrequencies[first][second];		
+			return prevHappy;	
 		}
 		else if(currentConfig.length() == 3) {
 			int first = hm.get(String.valueOf(currentConfig.charAt(0)));
 			int middle = hm.get(String.valueOf(currentConfig.charAt(1)));
 			int last = hm.get(String.valueOf(currentConfig.charAt(2)));
-			
-			fullHappy = threeLetterFrequencies[first][middle][last];
 
-			return (int)((fullHappy/dictionary_size) * 100);
+			return twoLetterFrequencies[first][middle] + twoLetterFrequencies[middle][last];
 			
 		}
 		else {
@@ -220,28 +129,6 @@ public class LetterCounter {
 		System.out.println(lc.happiness("a "));
 		System.out.println(lc.happiness("ax"));
 
-
-
-//		System.out.println("[]s : " +lc.happiness(" s"));
-//		System.out.println("[]d : " +lc.happiness(" d"));
-//		System.out.println("[]a : " +lc.happiness(" a"));
-//		System.out.println("[]i : " +lc.happiness(" i"));
-//		System.out.println("sa : " +lc.happiness("sa"));
-//		System.out.println("sd : " +lc.happiness("sd"));
-//		System.out.println("si : " +lc.happiness("si"));
-//		System.out.println("da : " +lc.happiness("da"));
-//		System.out.println("di : " +lc.happiness("di"));
-//		System.out.println("ds : " +lc.happiness("ds"));
-//		System.out.println("ai : " +lc.happiness("ai"));
-//		System.out.println("ad : " +lc.happiness("ad"));
-//		System.out.println("as : " +lc.happiness("as"));
-//		System.out.println("ia : " +lc.happiness("ia"));
-//		System.out.println("id : " +lc.happiness("id"));
-//		System.out.println("is : " +lc.happiness("is"));
-//		System.out.println("s[] : " +lc.happiness("s "));
-//		System.out.println("d[] : " +lc.happiness("d "));
-//		System.out.println("a[] : " +lc.happiness("a "));
-//		System.out.println("i[] : " +lc.happiness("i "));
 
 
 
